8e05ba3c2bd0d32f2b8c9c772e73147cd7a8b310
hrvstr <r.muhler@me.com>
2022-11-29 15:31:26 +0100

    Dynamically create operator keys

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const display = document.getElementById("display");
 const numBlock = document.getElementById("numBlock");
+const operators = document.getElementById("operators");
 
 // Create number keys
 for (i = 9; i >= 0; i--) {
   const div = document.createElement("div");
   const keyNumber = i;
   div.classList.add("key");
-  div.classList.add("numKey");
   div.setAttribute("id", "num" + keyNumber);
   div.textContent = keyNumber;
   numBlock.appendChild(div);
@@ -16,3 +16,35 @@ for (i = 9; i >= 0; i--) {
     display.textContent += keyNumber;
   });
 }
+
+// Create operator keys
+const operatorArray = [
+  {
+    name: "divide",
+    symbol: "÷",
+  },
+  {
+    name: "multiply",
+    symbol: "×",
+  },
+  {
+    name: "subtract",
+    symbol: "-",
+  },
+  {
+    name: "add",
+    symbol: "+",
+  },
+  {
+    name: "equal",
+    symbol: "=",
+  },
+];
+
+operatorArray.forEach((operator) => {
+  const div = document.createElement("div");
+  div.classList.add("key");
+  operators.appendChild(div);
+  div.setAttribute("id", operator.name);
+  div.textContent = operator.symbol;
+});
