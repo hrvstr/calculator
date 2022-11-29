c90775ea57e089638692e99bf4771f4f9ca29c2b
hrvstr <r.muhler@me.com>
2022-11-29 11:26:23 +0100

    Add operator and function keys

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+const display = document.getElementById("display");
 const numBlock = document.getElementById("numBlock");
 
+// Create number keys
 for (i = 9; i >= 0; i--) {
   const div = document.createElement("div");
-  div.classList.add("numKeys");
-  div.setAttribute("id", "num" + i);
-  div.textContent = i;
+  const keyNumber = i;
+  div.classList.add("key");
+  div.classList.add("numKey");
+  div.setAttribute("id", "num" + keyNumber);
+  div.textContent = keyNumber;
   numBlock.appendChild(div);
+
+  // Add number to display
+  div.addEventListener("click", () => {
+    display.textContent += keyNumber;
+  });
 }
